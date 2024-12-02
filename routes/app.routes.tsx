8a8adfdaac3401b05021a95540881527b94b2b4e
Paloma Cristina <paloma1899@gmail.com>
2024-12-01 23:01:16 -0300

    import register and edit refs

--- routes/app.routes.tsx
+++ routes/app.routes.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ActivityIndicator, View } from "react-native";
@@ -7,9 +7,11 @@ import { PostsProvider } from "@/context/Posts/PostsContext";
 import { UsersProvider } from "@/context/Users/UsersContext";
 import { PrivateRoute } from "@/components/shared/PrivateRoute";
 import Header from "@/components/shared/Header";
-
-const Home = lazy(() => import("../app/screens/Home/index"));
-const Login = lazy(() => import("../app/screens/Login/index"));
+import RegisterPostScreen from '../app/screens/RegisterPost'; 
+import EditPostScreen from '../app/screens/EditPostScreen'; 
+// Removendo o lazy loading e importando as telas diretamente
+import Home from "../app/screens/Home/index";
+import Login from "../app/screens/Login/index";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -27,34 +29,34 @@ export default function AppRoutes() {
     <AuthUserProvider>
       <PostsProvider>
         <UsersProvider>
-            <Suspense fallback={<LoadingComponent />}>
-              <Stack.Navigator
-                screenOptions={{
-                  headerShown: false,
-                }}
-              >
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Main">
-                  {() => (
-                    <PrivateRoute allowedRoles={["teacher", "student"]}>
-                      <Tab.Navigator>
-                        <Tab.Screen
-                          name="Home"
-                          options={{ headerShown: false }}
-                        >
-                          {() => (
-                            <>
-                              <Header />
-                              <Home/>
-                            </>
-                          )}
-                        </Tab.Screen>
-                      </Tab.Navigator>
-                    </PrivateRoute>
-                  )}
-                </Stack.Screen>
-              </Stack.Navigator>
-            </Suspense>
+          <Suspense fallback={<LoadingComponent />}>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Main">
+                {() => (
+                  <PrivateRoute allowedRoles={["teacher", "student"]}>
+                    <Tab.Navigator>
+                      <Tab.Screen
+                        name="Home"
+                        options={{ headerShown: false }}
+                      >
+                        {() => (
+                          <>
+                            <Header />
+                            <Home />
+                          </>
+                        )}
+                      </Tab.Screen>
+                    </Tab.Navigator>
+                  </PrivateRoute>
+                )}
+              </Stack.Screen>
+            </Stack.Navigator>
+          </Suspense>
         </UsersProvider>
       </PostsProvider>
     </AuthUserProvider>
